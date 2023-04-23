9d75b31cb9c2bcf99444694c20fe73aea7ba1bb0
katayama8000 <tattu.0310@gmail.com>
2023-04-23 19:31:49 +0900

    zenn

---
+++ src/zenn/zenn1.test.ts
@@ -0,0 +1,30 @@
+describe('zenn1', () => {
+  const mockFunction = jest.fn();
+  console.log(mockFunction.mock);
+  //   console.log(mock);
+  test('mock', () => {
+    expect(mockFunction).not.toHaveBeenCalled();
+  });
+
+  test('mock called once', () => {
+    mockFunction();
+    expect(mockFunction.mock.calls.length).toBe(1);
+    // mock解除
+    mockFunction.mockClear();
+  });
+
+  test('mock called with arguments', () => {
+    mockFunction('foo', 'bar', 'baz');
+    expect(mockFunction).toHaveBeenCalledWith('foo', 'bar', 'baz');
+    console.log(mockFunction.mock.calls);
+    expect(mockFunction.mock.calls[0]).toEqual(['foo', 'bar', 'baz']);
+  });
+
+  test('mock return value', () => {
+    mockFunction.mockReturnValueOnce('hello');
+    mockFunction.mockReturnValueOnce('goodbye');
+    expect(mockFunction()).toBe('hello');
+    expect(mockFunction()).toBe('goodbye');
+    expect(mockFunction()).toBe(undefined);
+  });
+});
