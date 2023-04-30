9a0bc2f5184f17ed557c79ca450d101234e5a99b
katayama8000 <tattu.0310@gmail.com>
2023-04-30 18:26:33 +0900

    mock

--- src/chapter3/users.test.ts
+++ src/chapter3/users.test.ts
@@ -18,11 +18,11 @@ describe('getNameList', () => {
     expect(actual).toEqual(expected);
   });
 
-  test('mock getNameList', async () => {
+  test('test1', async () => {
     const expected = ['山田 太郎', '佐藤 花子'];
 
     // getUsers関数をモック化
-    const mockGetUsers = jest.spyOn(users, 'getUsers').mockResolvedValue([
+    jest.spyOn(users, 'getUsers').mockResolvedValue([
       { name: '山田 太郎', age: 30, isDileted: false },
       { name: '佐藤 花子', age: 20, isDileted: false },
     ]);
@@ -35,5 +35,38 @@ describe('getNameList', () => {
     const actual = await users.getNameList();
     // expect(mockGetUsers).toHaveBeenCalledTimes(1);
     expect(actual).toEqual(expected);
+  });
+
+  test('test1', async () => {
+    // const expected = ['山田 太郎', '佐藤 花子'];
+    // // getUsers関数をモック化
+    // users.getUsers = jest.fn().mockResolvedValue([
+    //   { name: '山田 太郎', age: 30, isDileted: false },
+    //   { name: '佐藤 花子', age: 20, isDileted: false },
+    // ])
+    // // getNameList関数をモック化
+    // users.getNameList = jest.fn().mockResolvedValue(['山田 太郎', '佐藤 花子']);
+    // const actual = await users.getNameList();
+    // expect(actual).toEqual(expected);
+    expect(1).toBe(1);
+  });
+
+  test('test3', async () => {
+    //jest.mockとjest.spyOnの違い
+    //jest.mockはモジュール全体をモック化する
+    //jest.spyOnはモジュールの一部をモック化する
+
+    // test1をjest.mockを使って書き換える
+    // const expected = ['山田 太郎', '佐藤 花子'];
+    // jest.mock('./users', () => ({
+    //   getUsers: jest.fn().mockResolvedValue([
+    //     { name: '山田 太郎', age: 30, isDileted: false },
+    //     { name: '佐藤 花子', age: 20, isDileted: false },
+    //   ]),
+    //   getNameList: jest.fn().mockResolvedValue(['山田 太郎', '佐藤 花子']),
+    // }));
+    // const actual = await users.getNameList();
+    // expect(actual).toEqual(expected);
+    expect(1).toBe(1);
   });
 });
